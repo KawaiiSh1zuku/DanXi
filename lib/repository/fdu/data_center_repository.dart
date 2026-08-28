31aa391893b05efa39100f27b506ea43e32c95b7
Sh1zuku <51824296+KawaiiSh1zuku@users.noreply.github.com>
2026-08-28 21:20:06 +0800

    fix: try to fix electricity parse

--- lib/repository/fdu/data_center_repository.dart
+++ lib/repository/fdu/data_center_repository.dart
@@ -45,7 +45,9 @@ class DataCenterRepository extends BaseRepositoryWithDio {
   /// Divide canteens into different zones.
   /// e.g. 光华，南区，南苑，枫林，张江（枫林和张江无明显划分）
   Map<String?, Map<String, TrafficInfo>> toZoneList(
-      String? areaName, Map<String, TrafficInfo>? trafficInfo) {
+    String? areaName,
+    Map<String, TrafficInfo>? trafficInfo,
+  ) {
     Map<String?, Map<String, TrafficInfo>> zoneTraffic = {};
     if (trafficInfo == null) return zoneTraffic;
 
@@ -89,12 +91,16 @@ class DataCenterRepository extends BaseRepositoryWithDio {
       responseType: ResponseType.plain,
     );
     return FudanSession.request(
-        options, (req) => _parseCrowdednessInfo(req.data, areaCode),
-        type: FudanLoginType.Neo2FA);
+      options,
+      (req) => _parseCrowdednessInfo(req.data, areaCode),
+      type: FudanLoginType.Neo2FA,
+    );
   }
 
   Map<String, TrafficInfo> _parseCrowdednessInfo(
-      String responseData, int areaCode) {
+    String responseData,
+    int areaCode,
+  ) {
     var result = <String, TrafficInfo>{};
 
     //If it's not time for a meal
@@ -110,15 +116,20 @@ class DataCenterRepository extends BaseRepositoryWithDio {
         .replaceAll(r"\n", "-");
     var jsonExtraction = RegExp(r'\[.+?\]').allMatches(dataString);
     List<dynamic> names = jsonDecode(
-        jsonExtraction.elementAt(areaCode * 3).group(0)!.replaceAll("'", "\""));
-    List<dynamic>? cur = jsonDecode(jsonExtraction
-        .elementAt(areaCode * 3 + 1)
-        .group(0)!
-        .replaceAll("'", "\""));
-    List<dynamic>? max = jsonDecode(jsonExtraction
-        .elementAt(areaCode * 3 + 2)
-        .group(0)!
-        .replaceAll("'", "\""));
+      jsonExtraction.elementAt(areaCode * 3).group(0)!.replaceAll("'", "\""),
+    );
+    List<dynamic>? cur = jsonDecode(
+      jsonExtraction
+          .elementAt(areaCode * 3 + 1)
+          .group(0)!
+          .replaceAll("'", "\""),
+    );
+    List<dynamic>? max = jsonDecode(
+      jsonExtraction
+          .elementAt(areaCode * 3 + 2)
+          .group(0)!
+          .replaceAll("'", "\""),
+    );
     for (int i = 0; i < names.length; i++) {
       result[names[i]] = TrafficInfo(int.parse(cur![i]), int.parse(max![i]));
     }
@@ -169,27 +180,30 @@ class DataCenterRepository extends BaseRepositoryWithDio {
       final data = req.data as Map<String, dynamic>;
       return data["data"]
           .map<CardDetailInfo>(
-              (e) => CardDetailInfo.fromList(List<String>.from(e)))
+            (e) => CardDetailInfo.fromList(List<String>.from(e)),
+          )
           .toList();
     }, type: FudanLoginType.Neo2FA);
   }
 
   Future<List<ElectricityHistoryItem>> getElectricityHistory(
-      int offset, int size) {
+    int offset,
+    int size,
+  ) {
     final options = RequestOptions(
-        method: "POST",
-        path: ELECTRICITY_HISTORY_URL,
-        responseType: ResponseType.json,
-        data: FormData.fromMap({
-          "draw": 2,
-          "start": offset,
-          "length": size,
-        }));
+      method: "POST",
+      path: ELECTRICITY_HISTORY_URL,
+      responseType: ResponseType.json,
+      data: FormData.fromMap({"draw": 2, "start": offset, "length": size}),
+    );
     return FudanSession.request(options, (req) {
       final data = req.data as Map<String, dynamic>;
       return data["data"]
           .map<ElectricityHistoryItem>(
-              (e) => ElectricityHistoryItem.fromList((e as List).map((item) => item.toString()).toList()))
+            (e) => ElectricityHistoryItem.fromList(
+              (e as List).map((item) => item.toString()).toList(),
+            ),
+          )
           .toList();
     }, type: FudanLoginType.Neo2FA);
   }
@@ -208,17 +222,24 @@ class CardDetailInfo {
   String expireDate;
   String balance;
 
-  CardDetailInfo(this.id, this.name, this.status, this.permission,
-      this.expireDate, this.balance);
+  CardDetailInfo(
+    this.id,
+    this.name,
+    this.status,
+    this.permission,
+    this.expireDate,
+    this.balance,
+  );
 
   factory CardDetailInfo.fromList(List<String> elements) {
     return CardDetailInfo(
-        elements[0].trim(),
-        elements[1].trim(),
-        elements[2].trim(),
-        elements[3].trim(),
-        elements[4].trim(),
-        elements[5].trim());
+      elements[0].trim(),
+      elements[1].trim(),
+      elements[2].trim(),
+      elements[3].trim(),
+      elements[4].trim(),
+      elements[5].trim(),
+    );
   }
 }
 
@@ -230,7 +251,7 @@ class TrafficInfo {
 }
 
 class ElectricityHistoryItem {
-  /// Dorm name, e.g. "邯郸/15号楼/1501"
+  /// Dorm name, e.g. "邯郸校区/南区/30号楼/211"
   String dormName;
 
   /// Date of the record, e.g. "2024-06-01"
@@ -243,7 +264,7 @@ class ElectricityHistoryItem {
 
   factory ElectricityHistoryItem.fromList(List<String> elements) {
     return ElectricityHistoryItem(
-      '${elements[1].trim()}/${elements[2].trim()}/${elements[3].trim()}',
+      '${elements[0].trim()}/${elements[1].trim()}/${elements[2].trim()}/${elements[3].trim()}',
       elements[4].trim(),
       elements[5].trim(),
     );
